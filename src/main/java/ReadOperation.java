1735caa45ae3530a9e7753c49d958dd5def6b4d3
srijansa <159839387+srijansa@users.noreply.github.com>
2024-12-25 23:14:09 -0500

    Read Operation Added

---
+++ src/main/java/ReadOperation.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class ReadOperation {
+    public void readAll() throws SQLException{
+        String query = "select * from student";
+        try(
+                Connection conn = DatabaseConnection.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                ){
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println("Record: "+id + " " + name + " " + email);
+            }
+        }
+    }
+}
